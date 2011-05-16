8b2fd35d5944869e8bfd6c93ad6c4e2a3b97a702
Duncan <duncan@dcafsnelling.co.uk>
2011-05-17 00:36:19 +0800

    changed storage to S3

--- app/uploaders/library_doc_uploader.rb
+++ app/uploaders/library_doc_uploader.rb
@@ -7,8 +7,8 @@ class LibraryDocUploader < CarrierWave::Uploader::Base
   #     include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader
-  storage :file
-  #     storage :s3
+  #storage :file
+  storage :s3
 
   # set cache directory to writable temp directory (for Heroku)
   def cache_dir
@@ -17,9 +17,9 @@ class LibraryDocUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  #def store_dir
+  #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded
   #     def default_url
