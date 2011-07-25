70295399fb97bc9dc2cae445d4d495c4aca35546
Duncan <duncan@dcafsnelling.co.uk>
2011-07-26 00:58:33 +0800

    adding require dsl_definition to Rakefile

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -20,7 +20,7 @@ CarrierWave.configure do |config|
     :provider => 'AWS',
 	:aws_access_key_id => ENV['S3_ACCESS_KEY'],
 	:aws_secret_access_key => ENV['S3_ACCESS_KEY'],
-	:region => 'us-east-1'   # need to check
+	:region => 'us-east-1'
 	}
   config.fog_directory = ENV['S3_BUCKET_NAME']
   #config.fog_public = 'false'
